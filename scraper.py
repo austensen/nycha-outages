e6ab0562e286eb90b13079af38990a9a65347d1b
Maxwell Austensen <m.austensen@gmail.com>
2023-01-29 17:13:34 -0500

    update daily scraper script for new function name

--- scraper.py
+++ scraper.py
@@ -2,23 +2,23 @@ import requests
 import os
 from bs4 import BeautifulSoup
 
-from lib.parsers import scrape_outages
+from lib.daily_scrape import scrape_daily_outages
 
 URL = 'https://my.nycha.info/Outages/Outages.aspx'
 page = requests.get(URL)
 content = page.content
 soup = BeautifulSoup(content, 'html.parser')
 
-scrape_outages(soup, 'heatHotWater', 'Planned')
-scrape_outages(soup, 'heatHotWater', 'ClosedIn24Hours')
-scrape_outages(soup, 'heatHotWater', 'Open')
+scrape_daily_outages(soup, 'heatHotWater', 'Planned')
+scrape_daily_outages(soup, 'heatHotWater', 'ClosedIn24Hours')
+scrape_daily_outages(soup, 'heatHotWater', 'Open')
 
-scrape_outages(soup, 'elevator', 'Planned')
-scrape_outages(soup, 'elevator', 'ClosedIn24Hours')
-scrape_outages(soup, 'elevator', 'Open')
+scrape_daily_outages(soup, 'elevator', 'Planned')
+scrape_daily_outages(soup, 'elevator', 'ClosedIn24Hours')
+scrape_daily_outages(soup, 'elevator', 'Open')
 
-scrape_outages(soup, 'electric', 'Planned')
-scrape_outages(soup, 'electric', 'ClosedIn24Hours')
-scrape_outages(soup, 'electric', 'Open')
+scrape_daily_outages(soup, 'electric', 'Planned')
+scrape_daily_outages(soup, 'electric', 'ClosedIn24Hours')
+scrape_daily_outages(soup, 'electric', 'Open')
 
-scrape_outages(soup, 'gas')
+scrape_daily_outages(soup, 'gas')
